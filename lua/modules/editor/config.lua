f7063df2a76a6a4ad975af8cf5a4d2e726eedfee
csyJoy <505007945@qq.com>
2023-02-02 23:25:23 +0800

    fix: remove close_buffer

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -584,13 +584,6 @@ function config.virtualtext()
 	})
 end
 
-function config.close_buffer()
-	require("close_buffers").setup({
-		filetype_ignore = {}, -- Filetype to ignore when running deletions
-		file_glob_ignore = {}, -- File name glob pattern to ignore when running deletions (e.g. '*.md')
-		file_regex_ignore = {}, -- File name regex pattern to ignore when running deletions (e.g. '.*[.]md')
-		preserve_window_layout = { "this", "nameless" }, -- Types of deletion that should preserve the window layout
-		next_buffer_cmd = nil, -- Custom function to retrieve the next buffer when preserving window layout
 function config.bigfile()
 	local ftdetect = {
 		name = "ftdetect",
