e28c9803deb73174878731776e14f91d085b8a36
csyJoy <505007945@qq.com>
2023-05-27 01:05:46 +0800

    feat(vim-tex): change latex to xelatex

--- lua/modules/configs/lang/vim-tex.lua
+++ lua/modules/configs/lang/vim-tex.lua
@@ -1,6 +1,13 @@
-return function() 
-    vim.g.vimtex_view_method = "skim"
+return function()
+	vim.g.vimtex_view_method = "skim"
 	vim.g.vimtex_view_general_viewer = "skim"
 	vim.g.vimtex_view_skim_sync = 1
 	vim.g.vimtex_view_skim_activate = 1
+	vim.g.vimtex_compiler_latexmk_engines = {
+		_ = "-xelatex",
+	}
+	vim.g.tex_comment_nospell = 1
+	vim.g.vimtex_compiler_progname = "nvr"
+	vim.g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
+	vim.g.vimtex_view_general_options_latexmk = "--unique"
 end
