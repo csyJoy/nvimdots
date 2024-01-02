3ba779b1efd9a871d4660e565d90d6cf95a34511
csyJoy <505007945@qq.com>
2024-01-03 07:45:39 +0800

    plugin(vim-tmux-navigator)

--- lua/modules/plugins/tool.lua
+++ lua/modules/plugins/tool.lua
@@ -181,6 +181,23 @@ tool["folke/neodev.nvim"] = {
 	config = require("tool.neodev"),
 }
 
+tool["christoomey/vim-tmux-navigator"] = {
+	cmd = {
+		"TmuxNavigateLeft",
+		"TmuxNavigateDown",
+		"TmuxNavigateUp",
+		"TmuxNavigateRight",
+		"TmuxNavigatePrevious",
+	},
+	keys = {
+		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+		{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+	},
+}
+
 tool["demo"] = {
 	cmd = "GenerateFlashcards",
 	dir = "/Users/csy/demo.nvim",
