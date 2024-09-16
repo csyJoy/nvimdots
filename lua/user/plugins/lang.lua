ed2ac72dd1bf91b4a9a9876ae79da623ca13a2e3
csyJoy <chenshuyan.joy@mail.nwpu.edu.cn>
2024-09-16 22:08:07 +0800

    remove unused plugins in template

--- lua/user/plugins/lang.lua
+++ lua/user/plugins/lang.lua
@@ -2,19 +2,4 @@ local lang = {}
 
 lang["chrisbra/csv.vim"] = { opt = true, ft = "csv" }
 
-lang["lervag/vimtex"] = {
-	ft = "tex",
-	config = require("user.configs.lang.vim-tex"),
-}
-
-lang["ShinKage/idris2-nvim"] = {
-	ft = "idris2",
-	config = require("user.configs.lang.idris"),
-	dependencies = { "neovim/nvim-lspconfig", "MunifTanjim/nui.nvim" },
-}
-
-lang["ashinkarov/nvim-agda"] = {
-	ft = "agda",
-}
-
 return lang
